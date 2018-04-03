b3cd6f609d85615424049b707c303ca6e781c770
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-03 13:33:22 -0700

    Add user auth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find(username: username)
+
+    if user && user.is_password(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
