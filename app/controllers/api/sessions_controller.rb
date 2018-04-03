05a87152e13d0fa34f816322ae3a88948ad5e1e7
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-03 15:21:57 -0700

    Add application controller helper methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,20 @@
 class Api::SessionsController < ApplicationController
-  def create
+  def new
+    render :login
+  end
 
+  def create
+    user = User.find_by_credentials(
+      username: params.user.username,
+      password: params.user.password
+    )
+
+    if user
+      sign_in(user)
+      render :show
+    else
+      render ['Invalid Username or Password'], status: 422
+    end
   end
 
   def delete
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session_token: session[:session_token])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_in(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
 end
