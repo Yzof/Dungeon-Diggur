8f4fd3cfc6534d453e0ea723e12e86d111546442
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-03 14:39:59 -0700

    Generate Session Controller and Fill in User Controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
-class UsersController < ApplicationController
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render :show
+    else
+      render ['Invalid Username or Password'], status: 422
+    end
+  end
+
+  def delete
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :session_token)
+  end
 end
