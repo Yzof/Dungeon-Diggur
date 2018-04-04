72d3e1e0ddef37d6e206a72318e76dad01d9fea5
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-03 19:30:57 -0700

    Add App and root component

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Za Warldo</h1>
+  </div>
+);
+
+export default App;
--- frontend/dungeon_diggur.jsx
+++ frontend/dungeon_diggur.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as action from './actions/session_actions';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   window.sampleUser = {username: "jeff", password:"password"};
   const root = document.getElementById("root");
   const store = configureStore();
 
-  window.store = store;
-  window.login = action.login;
-  window.signup = action.signup;
-  window.logout = action.logout;
-  ReactDOM.render(<h1>Hello World</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
