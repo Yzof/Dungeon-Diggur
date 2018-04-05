8b92234f2d6e5cbc2c9d601b14119a30edc73b78
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-05 11:31:06 -0700

    Tweaked Dropdown position and coloring

--- frontend/components/user_dropdown/user_dropdown.jsx
+++ frontend/components/user_dropdown/user_dropdown.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
 class UserDropdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.revealDropdown = this.revealDropdown.bind(this);
+    this.hideDropdown = this.hideDropdown.bind(this);
+  }
+
+  revealDropdown(event) {
+    event.stopPropagation(); // prevent event from being picked up by body
+  	$('.user-dropdown').removeClass('hidden');
+    $('.user-dropdown-button').off('click', this.revealDropdown);
+    $(document).on('click', this.hideDropdown);
+  }
+
+  hideDropdown() {
+  	$('.user-dropdown').addClass('hidden');
+    $('.gear-dropdown-button').on('click', this.revealDropdown);
+    $(document).off('click', this.hideDropdown);
+  }
+
   render() {
     const { user, logout } = this.props;
     //Needs to link to user profile
     return (
       <div className="account-username">
-        <p id="user-dropdown-button">{user.username}</p>
+        <p
+          id="user-dropdown-button"
+          onClick={this.revealDropdown}
+          >{user.username}</p>
         <div className="user-dropdown hidden">
           <ul className="user-dropdown-options">
             <li>Profile TODO</li>
