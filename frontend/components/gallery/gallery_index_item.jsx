d9a743310440fbff1f2fde67d510e2880bcace44
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-21 10:29:48 -0700

    Refactored Classes code

--- frontend/components/gallery/gallery_index_item.jsx
+++ frontend/components/gallery/gallery_index_item.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const GalleryIndexItem = ({ gallery }) => {
+const GalleryIndexItem = ({ gallery, animation }) => {
   // This will be the element targeted with the animation
-  // How will we get the different classes in?
   // Randomly select from an array with the different classes?
 
-  // We are going to need about 7 different classes, space 3 seconds apart
-  const classes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+  // We have moved the classes to the parent component
 
   return(
   <li className="main-gallery-item">
--- frontend/components/gallery/main_gallery.jsx
+++ frontend/components/gallery/main_gallery.jsx
@@ -7,10 +7,21 @@ class MainGallery extends React.Component {
   }
 
   render(){
+    // We will need to pass in the class here, then add it in the index item component
+    // We are going to need about 7 different classes, space 3 seconds apart
+    const classes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
     const galleryItems = this.props.galleries.map(
-      (gallery) => (
-        gallery.assets[0] ? <GalleryIndexItem key={`gallery-index-item-${gallery.id}`} gallery={gallery} /> : null
-      )
+      (gallery) => {
+        // We will find the class in here
+        // INcrement an index and key into the classes array
+        // We reworked this to reduce line length
+        if (gallery.assets[0]) {
+          return <GalleryIndexItem key={`gallery-index-item-${gallery.id}`} gallery={gallery} />;
+        } else {
+          return null;
+        }
+      }
     );
     return (
       <div className="main-gallery-container" id="main">
