20a58a01bd01078ee9c4bfc9846e7cbb810aeff0
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-05-21 10:24:39 -0700

    Detailed Classes

--- frontend/components/gallery/gallery_index_item.jsx
+++ frontend/components/gallery/gallery_index_item.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const GalleryIndexItem = ({ gallery }) => {
+  // This will be the element targeted with the animation
+  // How will we get the different classes in?
+  // Randomly select from an array with the different classes?
+
+  // We are going to need about 7 different classes, space 3 seconds apart
+  const classes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
   return(
   <li className="main-gallery-item">
     <Link to={`/galleries/${gallery.id}`}>
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,4 +1,3 @@
-
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
